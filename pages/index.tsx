c282c3d2a9b8364af1c884d350ce413329755db6
vitaovich <vitaliyalekhnovich@gmail.com>
2023-12-31 14:46:59 +0000

    add a car dealing area

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,20 +21,25 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex">
-
-          <div className="flex flex-row space-x-12">
-            <div className="flex flex-col items center">
-              {pyramid}
-            </div>
-            <div className="flex flex-col items center">
-              {pyramid}
-            </div>
-            <div className="flex flex-col items center">
-              {pyramid}
-            </div>
+      <div className="flex flex-col space-y-12 bg-green-100 p-12">
+        <div className="flex flex-row space-x-12">
+          <div className="flex flex-col items center">
+            {pyramid}
+          </div>
+          <div className="flex flex-col items center">
+            {pyramid}
+          </div>
+          <div className="flex flex-col items center">
+            {pyramid}
           </div>
         </div>
+        <div className="flex justify-center">
+          <div className="flex flex-row bg-green-300 p-8 space-x-16">
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
