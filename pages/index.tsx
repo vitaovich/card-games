d97ddb45b2f04b498a9208cc6a862a68de2268f1
vitaovich <vitaliyalekhnovich@gmail.com>
2023-12-31 14:38:15 +0000

    add initial three pyramids

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Card } from '@vitaovich/card-components'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const pyramid = <>
+    <div className="flex justify-center">
+      <Card />
+    </div>
+    <div className="flex justify-center -mt-16 space-x-12">
+      <Card />
+      <Card />
+    </div>
+    <div className="flex justify-center -mt-16 space-x-12">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </>
+
   return (
     <>
-      <h1 className="text-3xl">
-        Starter NextJS TailwindCSS
-      </h1>
+      <div className="flex">
+
+          <div className="flex flex-row space-x-12">
+            <div className="flex flex-col items center">
+              {pyramid}
+            </div>
+            <div className="flex flex-col items center">
+              {pyramid}
+            </div>
+            <div className="flex flex-col items center">
+              {pyramid}
+            </div>
+          </div>
+        </div>
     </>
   )
 }
